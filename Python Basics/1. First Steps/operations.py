2db2d4bf70d9475581a28cad1ccc226a2a6eb479
Svilen Atanasov <a.svilen@gmail.com>
2022-07-21 22:59:49 +0300

    Add files via upload

---
+++ Python Basics/1. First Steps/operations.py
@@ -0,0 +1,18 @@
+a = 50
+b = 2
+
+# 1. Събиране
+result = a + b
+# 2. Изваждане
+result = a - b
+# 3. Умножение
+result = a * b
+# 4. Деление
+result = a / b
+# 5. Целочислено деление
+result = a // b
+# 6. Модулно деление
+result = a % b
+# 7. Повдигане на степен
+result = a ** b
+print(result)
---
+++ Python Basics/1. First Steps/pet_shop.py
@@ -0,0 +1,6 @@
+dogs_food = int(input())
+cats_food = int(input())
+
+total_sum = dogs_food * 2.5 + cats_food * 4
+
+print(f"{total_sum} lv.")
