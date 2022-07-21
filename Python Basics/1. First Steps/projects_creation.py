db2d16ef2278a686b6539660bbab6ceb97de32bf
Svilen Atanasov <a.svilen@gmail.com>
2022-07-21 22:59:49 +0300

    Add files via upload

---
+++ Python Basics/1. First Steps/projects_creation.py
@@ -0,0 +1,15 @@
+# architect_name = input()
+# project_number = int(input())
+#
+# hours_to_complete = project_number * 3
+#
+# print(f"The architect {architect_name} will need {hours_to_complete} hours to complete {project_number} project/s.")
+
+dog_food = int(input())
+car_food = int(input())
+
+total_dog_food = dog_food * 2.5
+total_cat_food = car_food * 4
+total_expenses = total_dog_food + total_cat_food
+
+print(f"{total_expenses} lv.")
