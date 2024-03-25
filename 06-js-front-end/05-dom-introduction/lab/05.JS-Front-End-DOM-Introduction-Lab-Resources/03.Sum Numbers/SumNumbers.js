cbff316b210fd1a59c707c7287fc35455ae89487
asvilen <a.svilen@gmail.com>
2024-03-25 22:15:38 +0200

    [js/05/lab] add problem 3 solution

--- 06-js-front-end/05-dom-introduction/lab/05.JS-Front-End-DOM-Introduction-Lab-Resources/03.Sum Numbers/SumNumbers.js
+++ 06-js-front-end/05-dom-introduction/lab/05.JS-Front-End-DOM-Introduction-Lab-Resources/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,7 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    const firstNum = document.getElementById('num1')
+    const secondNum = document.getElementById('num2')
+    const sum = document.getElementById('sum')
+    
+    sum.value = Number(firstNum.value) + Number(secondNum.value)
 }
