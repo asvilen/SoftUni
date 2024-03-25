cbbf05b42f2710c36f0180968a1c6a6a563fcf22
asvilen <a.svilen@gmail.com>
2024-03-25 22:33:01 +0200

    [js/05/lab] add problem 5 solution

--- 06-js-front-end/05-dom-introduction/lab/05.JS-Front-End-DOM-Introduction-Lab-Resources/05.Colorize Table/ColorizeTable.js
+++ 06-js-front-end/05-dom-introduction/lab/05.JS-Front-End-DOM-Introduction-Lab-Resources/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,7 @@
 function colorize() {
-    // TODO
+    const evenTableRows = document.querySelectorAll('table tr:nth-child(2n)')
+
+    for (const row of evenTableRows) {
+        row.style.backgroundColor = 'teal'
+    }
 }
