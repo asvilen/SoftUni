a0f891e8a99974ef7f44d1effc7e3f8663d8433e
asvilen <a.svilen@gmail.com>
2024-03-25 21:26:59 +0200

    [js/05/lab] add problem 1 solution

--- 06-js-front-end/05-dom-introduction/lab/05.JS-Front-End-DOM-Introduction-Lab-Resources/01.Edit Element/EditElement.js
+++ 06-js-front-end/05-dom-introduction/lab/05.JS-Front-End-DOM-Introduction-Lab-Resources/01.Edit Element/EditElement.js
@@ -1,3 +1,5 @@
-function editElement() {
-    // TODO
+function editElement(htmlEl, match, replacer) {
+    while (htmlEl.textContent.includes(match)) {
+        htmlEl.textContent = htmlEl.textContent.replace(match, replacer)
+    };
 }
