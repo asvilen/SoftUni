c0b01b31f016e24a489b0d083f402befb9477b8d
asvilen <a.svilen@gmail.com>
2024-03-26 18:42:57 +0200

    [js/05/lab] add problem 6 solution

--- 06-js-front-end/05-dom-introduction/lab/05.JS-Front-End-DOM-Introduction-Lab-Resources/06.Sum Table/SumTable.js
+++ 06-js-front-end/05-dom-introduction/lab/05.JS-Front-End-DOM-Introduction-Lab-Resources/06.Sum Table/SumTable.js
@@ -1,3 +1,9 @@
 function sumTable() {
-
+    const numElements = document.querySelectorAll('tr td:last-of-type:not(#sum)')
+    let sumElement = document.getElementById('sum')
+    let sum = 0
+    for (const el of numElements) {
+        sum += Number(el.textContent)
+    }
+    sumElement.textContent = sum
 }
