0ea8ef5755ce2483868bf2f238db4bcdf1314f47
asvilen <a.svilen@gmail.com>
2024-03-31 21:04:53 +0300

    [js/05/lab] add problem 7 solution

--- 06-js-front-end/05-dom-introduction/lab/05.JS-Front-End-DOM-Introduction-Lab-Resources/07.Extract Parenthesis/ExtractParenthesis.js
+++ 06-js-front-end/05-dom-introduction/lab/05.JS-Front-End-DOM-Introduction-Lab-Resources/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,10 @@
 function extract(content) {
-
+    const text = document.getElementById(content).textContent;
+    const regexPattern = /\(([a-zA-z ]+)\)/g;
+    const matches = text.match(regexPattern)
+    let result = []
+    for (const match of matches) {
+        result.push(match.slice(1, -1))
+    }
+    return result.join("; ")
 }
