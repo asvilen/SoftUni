93c3ffa91fdf3e38d6fc1c2966fa770031586eaf
asvilen <a.svilen@gmail.com>
2024-04-06 21:08:19 +0300

    [js/05/ex] add problem 5 solution

--- 06-js-front-end/05-dom-introduction/exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
+++ 06-js-front-end/05-dom-introduction/exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
@@ -2,7 +2,18 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      const tbodyEl = document.querySelectorAll('.container tbody tr')
+      const searchTerm = document.getElementById('searchField');
+      console.log(searchTerm.value)
+      // let nodes = tbodyEl
+      // console.log(nodes);
+      for (i = 0; i < tbodyEl.length; i ++) {
+         tbodyEl[i].classList.remove('select')
+         if (tbodyEl[i].textContent.includes(searchTerm.value)) {
+            console.log(tbodyEl[i].textContent);
+            tbodyEl[i].classList.add('select')
+      }
+      }
 
    }
 }
