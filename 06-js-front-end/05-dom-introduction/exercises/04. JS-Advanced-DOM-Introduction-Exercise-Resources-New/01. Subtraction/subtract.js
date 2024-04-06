ff199786777ffc556b64c83909e0c8bce893aaac
asvilen <a.svilen@gmail.com>
2024-04-06 18:24:42 +0300

    [js/05/ex] add problem 1 solution

--- 06-js-front-end/05-dom-introduction/exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/01. Subtraction/subtract.js
+++ 06-js-front-end/05-dom-introduction/exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/01. Subtraction/subtract.js
@@ -1,3 +1,9 @@
 function subtract() {
-    console.log('TODO:...');
+    const firstNumber = document.getElementById("firstNumber").value;
+    const secondNumber = document.getElementById("secondNumber").value;
+    const resultElement = document.getElementById("result");
+
+    let product = Number(firstNumber) - Number(secondNumber)
+
+    resultElement.textContent = product;
 }
