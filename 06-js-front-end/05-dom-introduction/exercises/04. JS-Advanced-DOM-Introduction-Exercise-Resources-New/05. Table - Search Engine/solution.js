48d5ec2530daea9ecd4d8e197d1858d03141ba9d
asvilen <a.svilen@gmail.com>
2024-04-06 23:46:26 +0300

    [js/05/ex] add problem 6 solution

--- 06-js-front-end/05-dom-introduction/exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
+++ 06-js-front-end/05-dom-introduction/exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
@@ -5,12 +5,9 @@ function solve() {
       const tbodyEl = document.querySelectorAll('.container tbody tr')
       const searchTerm = document.getElementById('searchField');
       console.log(searchTerm.value)
-      // let nodes = tbodyEl
-      // console.log(nodes);
       for (i = 0; i < tbodyEl.length; i ++) {
          tbodyEl[i].classList.remove('select')
          if (tbodyEl[i].textContent.includes(searchTerm.value)) {
-            console.log(tbodyEl[i].textContent);
             tbodyEl[i].classList.add('select')
       }
       }
--- 06-js-front-end/05-dom-introduction/exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
+++ 06-js-front-end/05-dom-introduction/exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
@@ -1,3 +1,25 @@
 function solve() {
-  //TODO
+  // Refresh
+  const outputDiv = document.getElementById("output");
+  outputDiv.textContent = "";
+  // Get needed elements
+  const textAreaEl = document.getElementById('input');
+  const divOutputEl = document.getElementById('output');
+
+  let result = textAreaEl.value
+    .split(".")
+    .filter(el => !!el)
+    .reduce((result, sentence, i) => {
+      const resultIndex = Math.floor(i / 3)
+      if (!result[resultIndex]) {
+        result[resultIndex] = []
+      }
+      result[resultIndex].push(sentence.trim())
+      return result;
+    }, [])
+    .map(sentences => `<p>${sentences.join(". ")}.</p>`)
+    .join("\n")
+
+    divOutputEl.innerHTML = result
+  
 }
