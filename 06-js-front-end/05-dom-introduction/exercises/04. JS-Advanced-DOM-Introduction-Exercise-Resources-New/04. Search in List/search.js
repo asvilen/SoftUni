a587aed1c6259e2329df0a24ec66dd5744637324
asvilen <a.svilen@gmail.com>
2024-04-06 20:37:43 +0300

    [js/05/ex] add problem 4 solution

--- 06-js-front-end/05-dom-introduction/exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
+++ 06-js-front-end/05-dom-introduction/exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
@@ -1,3 +1,17 @@
 function search() {
-   // TODO
+   const searchTerm = document.getElementById('searchText').value
+   const townsEl = document.querySelectorAll('#towns li')
+   const resultEl = document.getElementById('result')
+   let numMatches = 0
+   for (let town of townsEl) {
+      town.style.fontWeight = 'normal';
+      town.style.textDecoration = 'none'
+      townName = town.textContent
+      if (searchTerm && townName.includes(searchTerm)) {
+         town.style.textDecoration = 'underline';
+         town.style.fontWeight = 'undebolderrline';
+         numMatches += 1
+      }
+   }
+   resultEl.textContent = `${numMatches} matches found`;
 }
