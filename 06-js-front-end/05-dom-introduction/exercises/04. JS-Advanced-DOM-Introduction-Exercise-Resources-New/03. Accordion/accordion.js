ae761d243674ea51127a3c75b5bedd65597f4f0d
asvilen <a.svilen@gmail.com>
2024-04-06 19:36:17 +0300

    [js/05/ex] add problem 3 solution

--- 06-js-front-end/05-dom-introduction/exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/03. Accordion/accordion.js
+++ 06-js-front-end/05-dom-introduction/exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+    let buttonElText = document.getElementsByClassName('button').item(0);
+    const extraElement = document.getElementById('extra');
+
+    if (buttonElText.textContent === "More") {
+        extraElement.style.display = 'block';
+        buttonElText.textContent = 'Less'
+    } else {
+        extraElement.style.display = 'none';
+        buttonElText.textContent = 'More'
+    }
 }
