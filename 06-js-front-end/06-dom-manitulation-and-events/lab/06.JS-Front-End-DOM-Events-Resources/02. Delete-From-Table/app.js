d3d1adc01da1ffb2a6bb6c5c02702924c81d8864
asvilen <a.svilen@gmail.com>
2024-04-07 21:10:49 +0300

    [js/06/lab] add problem 2 solution

--- 06-js-front-end/06-dom-manitulation-and-events/lab/06.JS-Front-End-DOM-Events-Resources/02. Delete-From-Table/app.js
+++ 06-js-front-end/06-dom-manitulation-and-events/lab/06.JS-Front-End-DOM-Events-Resources/02. Delete-From-Table/app.js
@@ -1,3 +1,26 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const trElement = document.querySelectorAll('tbody tr');
+    const resultElement = document.getElementById('result');
+    const inputText = document.querySelector('input[type=text]')
+    console.log(inputText.value);
+
+    const trArray = Array
+        .from(trElement)
+        .map(tr => {
+            return tr
+        });
+
+    let resultMsg = "Not found."
+    trArray.forEach(trElement => {
+        const rowData = trElement.querySelectorAll('td')
+        if (rowData[1].textContent === inputText.value) {
+            trElement.remove(rowData);
+            resultMsg = "Deleted."
+        }
+    });
+
+    resultElement.textContent = resultMsg
+
+    inputText.value = ''
+
 }
