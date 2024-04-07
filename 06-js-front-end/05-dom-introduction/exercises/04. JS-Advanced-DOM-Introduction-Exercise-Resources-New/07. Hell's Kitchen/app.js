3feb32a45febe42330b8feeb39c9641c4b1cfc5d
asvilen <a.svilen@gmail.com>
2024-04-07 18:32:08 +0300

    [js/05/ex] add problem 7/8 solution

--- 06-js-front-end/05-dom-introduction/exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
+++ 06-js-front-end/05-dom-introduction/exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
@@ -2,7 +2,51 @@ function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
    function onClick () {
-      //   TODO:
-      
+      const textAreaEl = document.getElementsByTagName("textarea")
+      const text = textAreaEl.item(0).value
+      const inputArray = text
+         .split('"')
+         .filter(el => !['[', ']', ','].includes(el))
+      let highestAvgSalary = 0
+      let highestSalary = 0
+      let highestAvgSalaryTeam = ''
+      let higherSalaryWorkersInfo = {}
+      for (const restInfo of inputArray) {
+         const [name, averageSalary, bestSalary, workersInfoObj] = getRestaurantStats(restInfo)
+         if (highestAvgSalary < averageSalary) {
+            [highestAvgSalaryTeam, highestAvgSalary, highestSalary, higherSalaryWorkersInfo] = [name, averageSalary, bestSalary, workersInfoObj];
+         }
+      }
+      // Best Restaurant
+      const bestRestaurantOutputEl = document.querySelectorAll("#bestRestaurant p").item(0);
+      bestRestaurantOutputEl.textContent = `Name: ${highestAvgSalaryTeam} Average Salary: ${highestAvgSalary.toFixed(2)} Best Salary: ${highestSalary.toFixed(2)}`;
+      // Best Workers
+      const workersOutputEl = document.querySelectorAll("#workers p").item(0);
+      let workersString = ''
+      for (const key in higherSalaryWorkersInfo) {
+         workersString += `Name: ${key} With Salary: ${higherSalaryWorkersInfo[key]} `
+      }
+      workersOutputEl.textContent = workersString
+
+      function getRestaurantStats(restArray) {
+         const restArraySplitted = restArray.split(" - ");
+         const name = restArraySplitted[0];
+         const workers = restArraySplitted[1].split(", ");
+         let totalSalaries = 0;
+         let bestSalary = 0
+         let workersInfoObj = {}
+         for (const workerInfo of workers) {
+            const workersInfoArray = workerInfo.split(" ")
+            const currentWorker = workersInfoArray[0]
+            const currentSalary = Number(workersInfoArray[1]);
+            workersInfoObj[currentWorker] = currentSalary
+            totalSalaries += currentSalary;
+            if (currentSalary > bestSalary) {
+               bestSalary = currentSalary
+            }
+         }
+         const averageSalary = (totalSalaries / workers.length)
+         return [name, averageSalary, bestSalary, workersInfoObj]
+      }
    }
 }
--- 06-js-front-end/05-dom-introduction/exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/08. Generate Report/report.js
+++ 06-js-front-end/05-dom-introduction/exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/08. Generate Report/report.js
@@ -1,3 +1,36 @@
 function generateReport() {
-    //TODO
+    const theadElement = document.querySelectorAll('thead tr th')
+    const tableRows = document.querySelectorAll('tbody tr')
+    const outputElement = document.getElementById('output')
+
+    const columns = Array
+        .from(theadElement)
+        .map(th => {
+            const checkbox = th.querySelector('input[type=checkbox]')
+            return {
+                name: th.textContent.toLowerCase().trim(),
+                status: checkbox.checked,
+            }
+        })
+
+    const reportData = Array
+        .from(tableRows)
+        .map(trElement => {
+            const tdElements = trElement.querySelectorAll('td')
+
+            const result = Array
+                .from(tdElements)
+                .reduce((result, value, i) => {
+                    const name = columns[i].name
+                    const isActive = columns[i].status
+                    if (isActive) {
+                        result[name] = value.textContent
+                        return result
+                    }
+                    return result
+                }, {})
+            return result
+        })
+
+    outputElement.value = JSON.stringify(reportData, null, 2)
 }
