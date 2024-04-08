6ac50e05332d1fb68fb34be2a86119b627dc8e9f
asvilen <a.svilen@gmail.com>
2024-04-08 21:45:52 +0300

    [js/06/lab] add problem 6 solution

--- 06-js-front-end/06-dom-manitulation-and-events/lab/06.JS-Front-End-DOM-Events-Resources/06. Dynamic-Validation/app.js
+++ 06-js-front-end/06-dom-manitulation-and-events/lab/06.JS-Front-End-DOM-Events-Resources/06. Dynamic-Validation/app.js
@@ -1,3 +1,15 @@
 function validate() {
-    console.log('TODO:...');
+    const emailInputElement = document.getElementById('email')
+
+    emailInputElement.addEventListener('change', () => {
+        const emailAddress = emailInputElement.value;
+        const pattern = /^[a-z]*@[a-z]*\.[a-z]*/g
+        const matches = emailAddress.match(pattern)
+        if (!matches) {
+            emailInputElement.classList.add('error')
+        }
+        if (matches) {
+            emailInputElement.classList.remove('error')
+        }
+    })
 }
