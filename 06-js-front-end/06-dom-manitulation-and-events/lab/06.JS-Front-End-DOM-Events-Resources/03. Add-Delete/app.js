0ade2e72ef0abd786854cac48036a1a408d33726
asvilen <a.svilen@gmail.com>
2024-04-08 16:08:00 +0300

    [js/06/lab] add problem 3 solution

--- 06-js-front-end/06-dom-manitulation-and-events/lab/06.JS-Front-End-DOM-Events-Resources/03. Add-Delete/app.js
+++ 06-js-front-end/06-dom-manitulation-and-events/lab/06.JS-Front-End-DOM-Events-Resources/03. Add-Delete/app.js
@@ -1,3 +1,24 @@
 function addItem() {
-    //TODO...
+    const inputElement = document.getElementById('newItemText')
+    if (inputElement.value) {
+        const ulElement = document.getElementById('items')
+        const newItemElement = document.createElement('li')
+
+
+        const deleteLinkElement = document.createElement('a');
+        deleteLinkElement.textContent = "[Delete]"
+        deleteLinkElement.href = "#"
+
+        newItemElement.textContent = inputElement.value
+        newItemElement.appendChild(deleteLinkElement)
+
+        // Add event listener to the delete link
+        deleteLinkElement.addEventListener("click", () => {
+            newItemElement.remove();
+        });
+
+
+        ulElement.appendChild(newItemElement)
+        inputElement.value = ''
+    }
 }
