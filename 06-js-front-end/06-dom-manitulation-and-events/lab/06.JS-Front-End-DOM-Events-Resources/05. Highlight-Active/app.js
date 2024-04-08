cb365f4fccc8ecfee300423a3c2e00c36d464572
asvilen <a.svilen@gmail.com>
2024-04-08 17:06:42 +0300

    [js/06/lab] add problem 5 solution

--- 06-js-front-end/06-dom-manitulation-and-events/lab/06.JS-Front-End-DOM-Events-Resources/05. Highlight-Active/app.js
+++ 06-js-front-end/06-dom-manitulation-and-events/lab/06.JS-Front-End-DOM-Events-Resources/05. Highlight-Active/app.js
@@ -1,3 +1,18 @@
 function focused() {
-    console.log('TODO:...');
-}
+    const divElement = document.querySelector("body div");
+    const divsList = Array
+        .from(divElement.children)
+
+    for (const div of divsList) {
+        
+        const inputEl = div.querySelector("input");
+        // Add focus class when focused
+        inputEl.addEventListener('focus', () => {
+            div.classList.add('focused')
+        })
+        // Remove focus class when blurred
+        inputEl.addEventListener('blur', () => {
+            div.classList.remove('focused')
+        })
+    }
+    }
