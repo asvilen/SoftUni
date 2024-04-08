77ae5bf2aecab43fc0bfde5019a602d92784c5c0
asvilen <a.svilen@gmail.com>
2024-04-08 22:30:40 +0300

    [js/06/lab] add problem 7 solution

--- 06-js-front-end/06-dom-manitulation-and-events/lab/06.JS-Front-End-DOM-Events-Resources/07. Shopping-Cart/solution.js
+++ 06-js-front-end/06-dom-manitulation-and-events/lab/06.JS-Front-End-DOM-Events-Resources/07. Shopping-Cart/solution.js
@@ -1,3 +1,44 @@
 function solve() {
-   //TODO...
+   const checkoutButtonElement = document.querySelector('.checkout');
+   const productsElement = document.querySelectorAll(".product");
+   const textareaElement = document.querySelector('textarea');
+   const productsArray = Array.from(productsElement);
+
+   let boughtProducts = {};
+   for (const productElement of productsArray) {
+      const addButtonElement = productElement.querySelector('.add-product');
+      const productName = productElement.getElementsByClassName('product-title').item(0).textContent;
+      const productPrice = Number(productElement.getElementsByClassName('product-line-price').item(0).textContent);
+
+      addButtonElement.addEventListener('click', () => {
+         // Add only if it does not exist already
+         if (!boughtProducts[productName]) {
+            // Add to object
+            boughtProducts[productName] = productPrice
+            // Add products to textarea
+            result = `Added ${productName} for ${productPrice.toFixed(2)} to the cart.\n`
+            textareaElement.textContent += result
+         }
+      })
+   }
+
+   checkoutButtonElement.addEventListener('click', () => {
+      // Calculate total & create products list
+      let list = [];
+      let sum = 0;
+      for (const product in boughtProducts) {
+         list.push(product)
+         sum += boughtProducts[product]
+      }
+      // Add result to text area
+      result = `You bought ${list.join(", ")} for ${sum.toFixed(2)}.`
+      textareaElement.textContent += result
+      // disable buttons (add, checkout)
+      for (const productElement of productsArray) {
+         const addButtonElement = productElement.querySelector('.add-product');
+         addButtonElement.disabled = true
+         checkoutButtonElement.disabled = true
+      }
+   })
+
 }
