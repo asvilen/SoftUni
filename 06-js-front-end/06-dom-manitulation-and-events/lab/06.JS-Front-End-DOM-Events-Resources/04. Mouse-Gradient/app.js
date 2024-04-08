5d1e57fcfe8337a1a22caf483fff7af2f8aad175
asvilen <a.svilen@gmail.com>
2024-04-08 16:38:40 +0300

    [js/06/lab] add problem 4 solution

--- 06-js-front-end/06-dom-manitulation-and-events/lab/06.JS-Front-End-DOM-Events-Resources/04. Mouse-Gradient/app.js
+++ 06-js-front-end/06-dom-manitulation-and-events/lab/06.JS-Front-End-DOM-Events-Resources/04. Mouse-Gradient/app.js
@@ -1,3 +1,13 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const gradientElement = document.getElementById('gradient');
+    const resultElement = document.getElementById('result');
+
+    gradientElement.addEventListener('mousemove', (event) => {
+        const width = event.target.clientWidth;
+        const x = event.offsetX;
+
+        const perc = Math.floor(x / width * 100) + "%";
+
+        resultElement.textContent = perc
+    })
 }
