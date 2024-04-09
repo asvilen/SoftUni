84b359ca0ae55fb76ab7dd38f02a20a90f817dd9
asvilen <a.svilen@gmail.com>
2024-04-09 17:11:35 +0300

    [js/06/ex] add problem 5 solution

--- 06-js-front-end/06-dom-manitulation-and-events/exercises/06.JS-Front-End-DOM-Events-Exercise-Resources/05. Encode-and-Decode-Messages/app.js
+++ 06-js-front-end/06-dom-manitulation-and-events/exercises/06.JS-Front-End-DOM-Events-Exercise-Resources/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,29 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const mainButtonElements = document.querySelectorAll('#main button');
+    const decodeButton = mainButtonElements.item(0);
+    const encodeButton = mainButtonElements.item(1);
+
+    decodeButton.addEventListener('click', () => {
+        const mainTAElements = document.querySelectorAll('#main textarea');
+        const taSend = mainTAElements.item(0).value;
+        mainTAElements.item(0).value = ''
+        mainTAElements.item(1).value = transformMessage(taSend, 1)
+
+    })
+
+    encodeButton.addEventListener('click', () => {
+        const mainTAElements = document.querySelectorAll('#main textarea');
+        const taReceive = mainTAElements.item(1).value;
+        mainTAElements.item(1).value = transformMessage(taReceive, -1);
+
+    })
+
+    function transformMessage(msg, offset) {
+        let encodedMSG = "";
+        for (const char of msg) {
+            const asciNum = char.charCodeAt(0) + offset
+            encodedMSG += String.fromCharCode(asciNum)
+        }
+        return encodedMSG
+    }
 }
