3fe9b6ff14e256e109014979cff2acaccdfcd058
asvilen <a.svilen@gmail.com>
2024-04-09 16:04:54 +0300

    [js/06/ex] add problem 3 solution

--- 06-js-front-end/06-dom-manitulation-and-events/exercises/06.JS-Front-End-DOM-Events-Exercise-Resources/03. Locked-Profile/app.js
+++ 06-js-front-end/06-dom-manitulation-and-events/exercises/06.JS-Front-End-DOM-Events-Exercise-Resources/03. Locked-Profile/app.js
@@ -1,3 +1,33 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const profileElements = document.querySelectorAll(".profile")
+
+    const profileArray = Array.from(profileElements)
+    for (const profile of profileArray) {
+            const button = profile.querySelector("button");
+            button.addEventListener('click', () => {
+                const status = profile.querySelector("input[type = radio]:checked").value;
+                const hiddenDiv = profile.querySelector("div");
+                // MASK
+                if (!hiddenDiv.id.includes("!")) {
+                    if (status === 'unlock') {
+                        // Mask info - add "!"
+                        hiddenDiv.id = "!" + hiddenDiv.id
+                        // Update button text
+                        button.textContent = 'Hide it'
+                        return;
+                    }
+                }
+                // UNMASK
+                if (hiddenDiv.id.includes("!")) {
+                    if (status === 'unlock') {
+                        // Unmask info - remove added "!"
+                        const originalID = hiddenDiv.id.slice(1)
+                        hiddenDiv.id = originalID
+                        // Update button text
+                        button.textContent = 'Show more'
+                        return;
+                    }
+                }
+            })
+    }
 }
