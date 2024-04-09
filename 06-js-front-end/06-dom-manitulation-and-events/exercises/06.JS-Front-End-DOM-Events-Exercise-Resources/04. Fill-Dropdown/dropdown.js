e3ec245b0b37512f3473190f857e7c17a77ab5e8
asvilen <a.svilen@gmail.com>
2024-04-09 16:15:24 +0300

    [js/06/ex] add problem 4 solution

--- 06-js-front-end/06-dom-manitulation-and-events/exercises/06.JS-Front-End-DOM-Events-Exercise-Resources/04. Fill-Dropdown/dropdown.js
+++ 06-js-front-end/06-dom-manitulation-and-events/exercises/06.JS-Front-End-DOM-Events-Exercise-Resources/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,18 @@
 function addItem() {
-    console.log('TODO:...');
+    const newTextElement = document.getElementById('newItemText');
+    const newValueElement = document.getElementById('newItemValue');
+    const selectElement = document.getElementById('menu');
+    const newOptionElement = createOption(newTextElement.value, newValueElement.value);
+
+    selectElement.appendChild(newOptionElement)
+
+    newTextElement.value = '';
+    newValueElement.value = '';
+
+    function createOption(text, value) {
+        const newOptionElement = document.createElement('option');
+        newOptionElement.textContent = text;
+        newOptionElement.value = value;
+        return newOptionElement
+    }
 }
