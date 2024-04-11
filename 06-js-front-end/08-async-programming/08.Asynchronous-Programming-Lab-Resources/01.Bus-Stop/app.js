4e41252db56201e64fe96097284a6a40fc0c9060
asvilen <a.svilen@gmail.com>
2024-04-11 17:54:38 +0300

    [js/08] add problem 1 solution

--- 06-js-front-end/08-async-programming/08.Asynchronous-Programming-Lab-Resources/01.Bus-Stop/app.js
+++ 06-js-front-end/08-async-programming/08.Asynchronous-Programming-Lab-Resources/01.Bus-Stop/app.js
@@ -1,3 +1,24 @@
 function getInfo() {
-    // TODO:
+    const url = "http://localhost:3030/jsonstore/bus/businfo/";
+    const stopID = document.getElementById('stopId').value;
+    const stopName = document.getElementById('stopName');
+    const busesUIElement = document.getElementById('buses');
+    busesUIElement.textContent = ''
+
+    fetch(url + stopID)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            const busStopName = data['name'];
+            const busesObj = data['buses'];
+
+            stopName.textContent = busStopName
+
+            for (const bus in busesObj) {
+                const listElement = document.createElement('li')
+                listElement.textContent = `Bus ${bus} arrives in ${busesObj[bus]} minutes`
+                busesUIElement.appendChild(listElement)
+            }            
+        })
+        .catch(stopName.textContent = "Error");
 }
