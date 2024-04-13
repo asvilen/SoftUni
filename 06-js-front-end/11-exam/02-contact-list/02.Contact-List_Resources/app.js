8aadf3610250401aca81262602935b07b596a482
asvilen <a.svilen@gmail.com>
2024-04-13 16:12:08 +0300

    [js/exam] add exam problem 2 solution and problem 3

--- 06-js-front-end/11-exam/02-contact-list/02.Contact-List_Resources/app.js
+++ 06-js-front-end/11-exam/02-contact-list/02.Contact-List_Resources/app.js
@@ -1,6 +1,120 @@
 window.addEventListener("load", solve);
 
 function solve() {
-    //...TODO
+  // HTML Elements
+    const addButtonElemnent = document.getElementById('add-btn');
+    const inputNameElement = document.getElementById('name');
+    const inputPhoneElement = document.getElementById('phone');
+    const inputSelectElement = document.getElementById('category');
+    const checkListElement = document.getElementById('check-list');
+    const contactListElement = document.getElementById('contact-list');
+
+    addButtonElemnent.addEventListener('click', () => {
+      const inputData = getInputs();
+      const listItemElement = document.createElement('li');
+      // Buttons
+      const buttonsDivElement = document.createElement('div');
+      const editButtonElement = document.createElement('button');
+      const saveButtonElement = document.createElement('button');
+      editButtonElement.classList.add('edit-btn');
+      editButtonElement.setAttribute("id", inputData.name);
+      saveButtonElement.classList.add('save-btn');
+      saveButtonElement.setAttribute("id", inputData.name);
+      buttonsDivElement.appendChild(editButtonElement);
+      buttonsDivElement.appendChild(saveButtonElement);
+      // Article
+      const articleContainerElement = document.createElement('article');
+      articleContainerElement.setAttribute("id", inputData.name);
+      const namePElement = document.createElement('p');
+      const phonePElement = document.createElement('p');
+      const categoryPElement = document.createElement('p');
+      namePElement.textContent = `name: ${inputData.name}`;
+      phonePElement.textContent = `phone: ${inputData.phone}`;
+      categoryPElement.textContent = `category: ${inputData.category}`;
+      articleContainerElement.appendChild(namePElement);
+      articleContainerElement.appendChild(phonePElement);
+      articleContainerElement.appendChild(categoryPElement);
+      // Append to List Item
+      listItemElement.appendChild(articleContainerElement);
+      listItemElement.appendChild(buttonsDivElement);
+      // Append to Unordered List
+      checkListElement.appendChild(listItemElement);
+      // Clear inputs
+      clearInputs();
+
+      editButtonElement.addEventListener('click', () => {
+        // Get values by id
+        const contactName = editButtonElement.id
+        const articleElement = document.getElementById(contactName)
+        const paragraphs = articleElement.children;
+        const name = paragraphs[0].textContent.split(": ")[1];
+        const phone = paragraphs[1].textContent.split(": ")[1];
+        const category = paragraphs[2].textContent.split(": ")[1];
+        // Populate Input fields
+        inputNameElement.value = name;
+        inputPhoneElement.value = phone;
+        inputSelectElement.value = category;
+        // Remove List Item
+        listItemElement.remove()
+      })
+
+      saveButtonElement.addEventListener('click', () => {
+        // Append to contact-list
+        // Get values by id
+        const contactName = saveButtonElement.id
+        const articleElement = document.getElementById(contactName)
+        const paragraphs = articleElement.children;
+        const name = paragraphs[0].textContent.split(": ")[1];
+        const phone = paragraphs[1].textContent.split(": ")[1];
+        const category = paragraphs[2].textContent.split(": ")[1];
+        const saveListItemElement = document.createElement('li');
+        // Buttons
+        const deleteButtonElement = document.createElement('button');
+        deleteButtonElement.classList.add('del-btn');
+        deleteButtonElement.setAttribute("id", name);
+        // Article
+        const articleContainerElement = document.createElement('article');
+        articleContainerElement.setAttribute("id", name);
+        const namePElement = document.createElement('p');
+        const phonePElement = document.createElement('p');
+        const categoryPElement = document.createElement('p');
+        namePElement.textContent = `name: ${name}`;
+        phonePElement.textContent = `phone: ${phone}`;
+        categoryPElement.textContent = `category: ${category}`;
+        articleContainerElement.appendChild(namePElement);
+        articleContainerElement.appendChild(phonePElement);
+        articleContainerElement.appendChild(categoryPElement);
+        // Append to List Item
+        saveListItemElement.appendChild(articleContainerElement);
+        saveListItemElement.appendChild(deleteButtonElement);
+        // Append to Unordered List
+        contactListElement.appendChild(saveListItemElement);
+
+        // Remove from check-list
+        listItemElement.remove()
+
+        deleteButtonElement.addEventListener('click', () => {
+          // Delete the element 
+          saveListItemElement.remove();
+        })
+      })
+    })
+
+    function getInputs() {
+      if (!inputNameElement.value || !inputPhoneElement.value || !inputSelectElement.value) {
+        return
+      }
+      return {
+        name: inputNameElement.value,
+        phone: inputPhoneElement.value,
+        category: inputSelectElement.value,
+      }
+    }
+
+    function clearInputs() {
+      inputNameElement.value = '';
+      inputPhoneElement.value = '';
+      inputSelectElement.value = '';
+    }
   }
   
