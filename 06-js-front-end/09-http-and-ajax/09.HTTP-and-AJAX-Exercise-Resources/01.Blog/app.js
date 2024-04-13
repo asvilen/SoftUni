af115f91d37656b7c33130f5b40c1bd7cd197d4a
asvilen <a.svilen@gmail.com>
2024-04-13 10:25:41 +0300

    [js/10] add problem set

--- 06-js-front-end/09-http-and-ajax/09.HTTP-and-AJAX-Exercise-Resources/01.Blog/app.js
+++ 06-js-front-end/09-http-and-ajax/09.HTTP-and-AJAX-Exercise-Resources/01.Blog/app.js
@@ -1,5 +1,54 @@
 function attachEvents() {
-    // TODO:
+    const baseURL = "http://localhost:3030/jsonstore/blog";
+    const postsResource = "/posts";
+    const commentsResource = "/comments";
+
+    // HTML Elements 
+    const loadButtonElement = document.getElementById('btnLoadPosts');
+    const selectElement = document.getElementById('posts');
+    const viewButtonElement = document.getElementById('btnViewPost');
+    const titleElement = document.getElementById('post-title');
+    const postElement = document.getElementById('post-body');
+    const commentsULElement = document.getElementById('post-comments');
+
+    loadButtonElement.addEventListener('click', () => {
+        fetch(baseURL + postsResource)
+            .then(response => response.json())
+            .then(data => {
+                for (const key in data) {
+                    // console.log(key);
+                    // console.log(data[key]);
+                    const optionElement = document.createElement('option');
+                    optionElement.value = key;
+                    optionElement.textContent = data[key].title;
+                    selectElement.appendChild(optionElement);
+                }
+            })
+        
+    })
+
+    viewButtonElement.addEventListener('click', () => {
+        const selectedValue = selectElement.value;
+        // Post title and body
+        fetch(baseURL + postsResource + "/" + selectedValue)
+            .then(response => response.json())
+            .then(post => {
+                titleElement.textContent = post.title;
+                postElement.textContent = post.body;
+            })
+        // Comments
+        fetch(baseURL + commentsResource)
+            .then(response => response.json())
+            .then(data => {
+                const comments = Object.values(data).filter(obj => obj['postId'] === selectedValue);
+                comments.forEach(obj => {
+                    const commentText = obj.text;
+                    const listElement = document.createElement('li');
+                    listElement.textContent = commentText;
+                    commentsULElement.appendChild(listElement);
+                })
+            })
+    })
 }
 
 attachEvents();
